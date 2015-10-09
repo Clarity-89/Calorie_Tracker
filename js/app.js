f1af3a3e924dd70791a0ed17d30eb707423d4751
Clarity-89 <homes89@ukr.net>
2015-10-09 23:26:59 +0300

    Set up file structure

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@
  * Created by Alex on 08/10/2015.
  */
 
+var app = app || {};
+var ENTER_KEY = 13;
 
+$(function() {
+
+    new app.ResultsView();
+
+});
--- js/views/results.js
+++ js/views/results.js
@@ -2,8 +2,49 @@
  * Created by Alex on 09/10/2015.
  */
 
+// Handle the view for the list of results returned after search button is pressed
 var app = app || {};
 
 app.ResultsView = Backbone.View.extend({
 
+    tagName: 'li',
+
+    template: Handlebars.compile($('#searchResults').html()),
+
+    events: {
+        'click #searchButton': 'search',
+        'click #addToList': 'addItem',
+        'keypress #search': 'search'
+    },
+
+    initialize: function () {
+        this.$searchButton = this.$('#searchButton');
+        this.$search = this.$('#search');
+        this.listenTo(app.ItemList, 'all', this.render);
+    },
+
+    render: function () {
+        console.log('render');
+        //var results = app.ItemList.length;
+
+        //if (results){
+        this.$el.html(template(this.model.toJSON()));
+        //}
+    },
+
+    search: function () {
+        console.log('search button pressed');
+        var query = this.$search.val().trim();
+        console.log(query);
+        var data = {
+            "appId": "13957b27",
+            "appKey": "634647fd3fadbe686dbaacdbea287beb",
+            "query": query
+        };
+
+        $.getJSON("https://api.nutritionix.com/v1_1/search -H 'Content-Type: application/json' -d'", data, function (res) {
+            console.log(res);
+        });
+    }
 });
+
